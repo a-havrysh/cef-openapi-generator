1518921c4fea635bfc0f484c47aa9b1feafd02a0
oleksandr.havrysh <oleksandr.havrysh@audienzz.com>
2026-01-04 15:51:38 +0100

    Add GitHub Packages publishing and CI/CD
    - Configure maven-publish for GitHub Packages
    - Add sources and javadoc JARs
    - Complete POM metadata
    - Add GitHub Actions workflow for auto-publish
    - Version 1.0.0 stable release

--- generator/build.gradle.kts
+++ generator/build.gradle.kts
@@ -4,10 +4,15 @@ plugins {
 }
 
 group = "io.github.cef"
-version = "1.0.0-SNAPSHOT"
+version = "1.0.0"
 
 dependencies {
     implementation("org.openapitools:openapi-generator:7.18.0")
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
 }
 
 publishing {
@@ -19,6 +24,43 @@ publishing {
                 archiveClassifier.set("templates")
                 from(projectDir.parent + "/templates")
             })
+
+            pom {
+                name.set("CEF OpenAPI Generator")
+                description.set("OpenAPI Code Generator for Chromium Embedded Framework with JetBrains Platform integration")
+                url.set("https://github.com/a-havrysh/cef-openapi-generator")
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("a-havrysh")
+                        name.set("Alexander Havrysh")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/a-havrysh/cef-openapi-generator.git")
+                    developerConnection.set("scm:git:ssh://github.com/a-havrysh/cef-openapi-generator.git")
+                    url.set("https://github.com/a-havrysh/cef-openapi-generator")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/a-havrysh/cef-openapi-generator")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR") ?: project.findProperty("gpr.user") as String?
+                password = System.getenv("GITHUB_TOKEN") ?: project.findProperty("gpr.token") as String?
+            }
         }
     }
 }
